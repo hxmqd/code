1501515d6643f8f1c186172d85b1e544fcff6e34
hxm <hexiaominqd@163.com>
2018-07-03 23:01:25 +0800

    add concurrent

---
+++ src/book/MinK.java
@@ -0,0 +1,62 @@
+package book;
+//无序数组中最小的K个数
+public class MinK {
+
+    public int[] getMinKNumsByHeap(int[] arr, int k){
+        if(k < 1 || k > arr.length){
+            return arr;
+        }
+        int[] kHeap = new int[k];
+        for(int i = 0; i != k; i++){
+            heapInsert(kHeap, arr[i], i);
+        }
+        for(int i = k; i != arr.length; i++){
+            if(arr[i] < kHeap[0]){
+                kHeap[0] = arr[i];
+                heapify(kHeap, 0, k);
+            }
+        }
+        return kHeap;
+    }
+
+    public void heapInsert(int[] arr, int value, int index){
+        arr[index] = value;
+        while(index != 0){
+            int parent = (index -1)/2;
+            if(arr[parent] < arr[index]){
+                swap(arr, parent, index);
+            } else {
+                break;
+            }
+        }
+    }
+
+    public void heapify(int[] arr, int index, int heapSize){
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+        int largest = index;
+        while(left < heapSize){
+            if(arr[left] > arr[index]){
+                largest = left;
+            }
+            if(right < heapSize && arr[right] > arr[largest]){
+                largest = right;
+            }
+            if(largest != index){
+                swap(arr, largest, index);
+            } else {
+                break;
+            }
+            index = largest;
+            left = index * 2 + 1;
+            right = index * 2 + 2;
+        }
+
+
+    }
+    public void swap(int[] arr, int index1, int index2){
+        int tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+}
---
+++ src/java_concurrent/ThreadLocalTest.java
@@ -0,0 +1,59 @@
+package java_concurrent;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+//ReentrantLock由最后成功获得但尚未释放锁的线程拥有，线程调用锁将返回，成功获取锁
+// 如果当前线程已拥有该锁，则lock()方法将立即返回
+public class ThreadLocalTest {
+
+    public static final ThreadLocal<Object> threadLocal = new ThreadLocal<Object>() {
+        protected Object initialValue() {
+            //调用get方法时，若当前线程共享变量没有设置，调用initialValue获取默认值
+            System.out.println(Thread.currentThread().getName()+" invoke initialValue");
+            return null;
+        }
+
+    };
+
+    private final ReentrantLock lock = new ReentrantLock();
+
+
+    public static void main(String[] args) {
+        ReentrantLockTask task = new ReentrantLockTask();
+
+        Thread thread1 = new Thread(task);
+        Thread thread2 = new Thread(task);
+
+        thread1.start();
+        thread2.start();
+
+
+
+    }
+}
+
+
+
+    class ReentrantLockTask implements Runnable {
+
+
+        @Override
+        public void run() {
+         for (int i = 0; i < 5; i++) {
+                if (null == ThreadLocalTest.threadLocal.get()) {
+                    ThreadLocalTest.threadLocal.set(0);
+                } else {
+                    int num = (Integer) ThreadLocalTest.threadLocal.get();
+                    ThreadLocalTest.threadLocal.set(num + 1);
+                    System.out.println(Thread.currentThread().getName() + ":" + ThreadLocalTest.threadLocal.get());
+                }
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
